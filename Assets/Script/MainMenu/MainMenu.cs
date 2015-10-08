2affe5186505dd6677edd5040e99005205030efe
jiminl5 <jiminl5@users.noreply.github.com>
2015-10-08 12:53:40 -0700

    Updated MainMenu

--- Assets/Script/MainMenu/MainMenu.cs
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -3,31 +3,95 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-    public Texture backgroundTexture;
+	//Background Texture
+    //public Texture backgroundTexture;
 
+	//GUI Button Texture
+	public Texture texture_Tavern;
+	public Texture texture_Potion;
+	public Texture texture_Settings;
+
+	//Non-Static Button Coordinate Variable (X)
+	public float button_tavern_X = .25f; // Xs are initialized at .25f
+	public float button_setting_X = .25f;
+	public float button_potion_X = .25f;
+
+	//Non-Static Button Coordinate Variable (Y)
+	public float button_tavern_Y;
     public float button_setting_Y;
-    public float button_exit_Y;
+    public float button_potion_Y;
+
+	//Non-Static Button Size Variable (W)
+	public float button_tavern_W;
+	public float button_setting_W;
+	public float button_potion_W;
+
+	//Non-Static Button Size Variable (H)
+	public float button_tavern_H;
+	public float button_setting_H;
+	public float button_potion_H;
 
     public string scene_name;
+
+	//Cam Size & Zoom effects
+	public float camSize;
+	public float camSizeLimit;
+	public bool ZoomIn = false;
+	public float increment;
+	
+	void Start()
+	{
+		camSize = Camera.main.orthographicSize; // initialize camsize
+	}
+
+	void Update()
+	{
+		ZoomCamera ();
+	}
 
     void OnGUI()
     {
         //Background Texture
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+        //GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
 
         //GUI Buttons
-        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "PLAY"))
+		if (GUI.Button(new Rect(Screen.width * button_tavern_X, Screen.height * button_tavern_Y,
+		                        Screen.width * button_tavern_W, Screen.height * button_tavern_H), texture_Tavern, "")) // No Text, Only Texture in the future
         {
-            Application.LoadLevel(scene_name);
+			//print (this.transform.position);
+			ZoomIn = true;
+			//ZoomCamera();
+            //Application.LoadLevel(scene_name);
         }
-        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * button_setting_Y, Screen.width * .5f, Screen.height * .1f), "Setting"))
+		if (GUI.Button(new Rect(Screen.width * button_setting_X, Screen.height * button_setting_Y,
+		                        Screen.width * button_setting_W, Screen.height * button_setting_H), texture_Settings, ""))
         {
 			Application.LoadLevel ("options");
         }
-        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * button_exit_Y, Screen.width * .5f, Screen.height * .1f), "Exit"))
+		if (GUI.Button(new Rect(Screen.width * button_potion_X, Screen.height * button_potion_Y,
+		                        Screen.width * button_potion_W, Screen.height * button_potion_H), texture_Potion, ""))
         {
 
         }
     }
+
+	void ZoomCamera()
+	{
+		if (ZoomIn) {
+			Camera.main.orthographicSize = 
+				Mathf.Lerp (Camera.main.orthographicSize,
+				            Camera.main.orthographicSize - increment,
+				            Time.deltaTime * 2);
+
+			Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y - 0.048f, transform.position.z); // Fix Camera position while zoom (to Tavern Door)
+
+			print (Camera.main.orthographicSize);
+
+			if (Camera.main.orthographicSize <= 0.1f) {
+				ZoomIn = false;
+				Application.LoadLevel(scene_name);
+			}
+		}
+	}
 
 }
