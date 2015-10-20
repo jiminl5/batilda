6c97d20a32c0428d1872c904b2c49960847b8d82
Chris Fulgencio <fulgencc@uci.edu>
2015-10-20 13:53:42 -0700

    chef v1.0
    fixed

---
+++ Assets/Script/gameScene v2.0/Furnace.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Furnace : MonoBehaviour {
+	public bool isOn = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator ExecuteAfterDelay(float delay)
+	{
+		isOn = true;
+		//update sprite;
+		yield return new WaitForSeconds(delay);
+		isOn = false;
+		//update sprite;
+	}
+}
