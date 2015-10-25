b51c94623922b8d06d67b8ecccaabe90fc6a814e
Warren Urbina <wurbina@uci.edu>
2015-10-25 09:38:09 -0700

    adding in More Menu/Select functionality

--- Assets/Script/MainMenu/LevelSelectionModal.cs
+++ Assets/Script/MainMenu/LevelSelectionModal.cs
@@ -4,44 +4,10 @@ using UnityEngine.Events;
 using System.Collections;
 
 public class LevelSelectionModal : MonoBehaviour {
-	public Button day;
-	public Button cancelBtn;
-    public Button popUpLevelSelect;
-	public GameObject levelSelectionObject;
-
-	private static LevelSelectionModal levelPanel;
-
-	public static LevelSelectionModal Instance(){
-		if (!levelPanel) {
-			levelPanel = FindObjectOfType(typeof (LevelSelectionModal)) as LevelSelectionModal;
-			if(!levelPanel)
-				Debug.LogError("An active Level Panel script is required for GameObject in your scene.");
-		}
-		return levelPanel;
-	}
-
-	public void Tempy(Button day, Button cancelBtn, UnityAction selectEvent, UnityAction cancelEvent){
-		levelSelectionObject.SetActive (true);
-
-		day.onClick.RemoveAllListeners ();
-		day.onClick.AddListener (selectEvent);
-		day.onClick.AddListener (CloseLevelSelectionPanel);
-
-		cancelBtn.onClick.RemoveAllListeners ();
-		cancelBtn.onClick.AddListener (cancelEvent);
-		cancelBtn.onClick.AddListener (CloseLevelSelectionPanel);
-
-		day.gameObject.SetActive (true);
-		cancelBtn.gameObject.SetActive (true);
-	}
-
-	void CloseLevelSelectionPanel(){
-		levelSelectionObject.SetActive (false);
-	}
-
-    void OpenLevelSelectionPanel()
+	
+    public void loadLevel(string level_number)
     {
-        levelSelectionObject.SetActive(true);
+        Application.LoadLevel("level" + level_number);
     }
     
 }
---
+++ Assets/Script/gameScene v2.0/LevelTimer/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopWatch : MonoBehaviour {
+
+    private float startTime;
+    private float restSeconds;
+    private float roundedRestSeconds;
+    private float displaySeconds;
+    private float displayMinutes;
+    public float countDownSeconds;
+
+	// Use this for initialization
+	void Awake () {
+        startTime = Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI() {
+        float guiTime = Time.time - startTime;
+        restSeconds = countDownSeconds - guiTime;
+
+        if(restSeconds == 60)
+        {
+            print("one min left");
+        }
+        if(restSeconds == 0)
+        {
+            print("times up");
+        }
+
+        roundedRestSeconds = Mathf.CeilToInt(restSeconds);
+        displaySeconds = roundedRestSeconds % 60;
+        displayMinutes = roundedRestSeconds / 60;
+        string text = string.Format("{0:00}:{1:00}", displayMinutes, displaySeconds);
+        GUI.Label(new Rect(400, 25, 100, 30), text);
+	}
+}
