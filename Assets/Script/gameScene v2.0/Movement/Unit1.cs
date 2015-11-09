78b02719b77a2ce3ce0e81d0a0d81d833a97c2df
jiminl5 <jiminl5@users.noreply.github.com>
2015-11-08 20:31:45 -0800

    Batilda's Movement Animation

--- Assets/Script/gameScene v2.0/Movement/Unit1.cs
+++ Assets/Script/gameScene v2.0/Movement/Unit1.cs
@@ -13,6 +13,12 @@ public class Unit1 : MonoBehaviour {
 
 	int tmp;
 
+	private int _bat_animState = Animator.StringToHash("bat_animState");
+	private Animator _animator;
+
+	public string left_right;
+	public string down_up;
+
 	//List
 	public List<Node> currentPath = null;
     public static List<Node> temp_list2 = null;
@@ -27,6 +33,7 @@ public class Unit1 : MonoBehaviour {
 	{
         temp_list2 = new List<Node>();
         g_object1 = GameObject.Find("Unit1");
+		_animator = this.GetComponentInChildren<Animator> ();
 	}
 
 	public void Update()
@@ -67,8 +74,51 @@ public class Unit1 : MonoBehaviour {
             Vector2 currentWayPoint = new Vector2(real_tileX, real_tileY);
 	        //transform.position = new Vector2(temp_path[0].x, temp_path[0].y);
 	        //grab next first node and move
-            if (g_object1.transform.position.x != currentWayPoint.x || g_object1.transform.position.y != currentWayPoint.y)
+			if (g_object1.transform.position.x != currentWayPoint.x || g_object1.transform.position.y != currentWayPoint.y)
             {
+				//MOVEMENT ANIMATION && First movement
+				if (g_object1.transform.position.x > currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y) //LEFT
+				{
+					_animator.SetInteger(_bat_animState, 1);
+					left_right = "left";
+				}
+				if (g_object1.transform.position.x < currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y) //RIGHT
+				{
+					_animator.SetInteger(_bat_animState, 2);
+					left_right = "right";
+				}
+				if (g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y > currentWayPoint.y) //DOWN
+				{
+					_animator.SetInteger(_bat_animState, 3);
+					down_up = "down";
+				}
+				if (g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y < currentWayPoint.y) //UP
+				{
+					_animator.SetInteger(_bat_animState, 4);
+					down_up = "up";
+				}
+
+				// 2 - Way Direction //for tenth digit 1: left first, 2: right first, 3: down first, 4: up first
+				if (left_right.Equals("left") && g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y > currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 11); // left then down
+				else if (left_right.Equals("left") && g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y < currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 12); // left then up
+				else if (left_right.Equals("right") && g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y > currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 21); // right then down
+				else if (left_right.Equals("right") && g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y < currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 22); // right then up
+
+				if (down_up.Equals("down") && g_object1.transform.position.x > currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 31); // Down then left
+				else if (down_up.Equals("down") && g_object1.transform.position.x < currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 32); // Down then right
+				else if (down_up.Equals("up") && g_object1.transform.position.x > currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 41);
+				else if (down_up.Equals("up") && g_object1.transform.position.x < currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y)
+					_animator.SetInteger(_bat_animState, 42);
+
+
+				//MOVE
                 g_object1.transform.position = Vector2.MoveTowards(g_object1.transform.position, currentWayPoint, Time.deltaTime * speed); // move toward not lerp
             }
 			if (g_object1.transform.position.x == currentWayPoint.x && g_object1.transform.position.y == currentWayPoint.y)
@@ -77,7 +127,10 @@ public class Unit1 : MonoBehaviour {
 			}
 			if (temp_path.Count == 1)
 			{
+				left_right = ""; // Make direction empty
+				down_up = "";
 				print("Succesfully at destination");
+				_animator.SetInteger(_bat_animState, 0);
 				mouseClicked = false;
 			}
 
