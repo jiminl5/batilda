7c1600b88a6aa225f13da35a8530105528056976
Chris Fulgencio <fulgencc@uci.edu>
2015-11-19 12:32:35 -0800

    unfinished Waitress

---
+++ Assets/Script/gameScene v2.0/Waitress.cs
@@ -0,0 +1,238 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waitress : MonoBehaviour {
+	public string one_h;
+	public string two_h;
+	public GameObject temp_plate;
+	public string at_current_name;
+	//public GameObject Player;
+	GameObject go_1h;
+	GameObject go_2h;
+	
+	public int mtX;
+	public int mtY;
+	public bool atPosition = true;
+	public static bool clicked = false;
+	
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		atPosition = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("mtX: " + mtX + " mtY: " + mtY + " atPosition = " + atPosition);
+		if (findGameObjectAtClickedPosition ()) {
+			GameObject go = findGameObjectAtClickedPosition ();
+			//Debug.Log (go);
+			animator.SetBool("1_h", !string.IsNullOrEmpty(one_h));
+			animator.SetBool("2_h", !string.IsNullOrEmpty(two_h));
+			if (!clicked) {
+			} else {
+				//				Debug.Log (!string.IsNullOrEmpty(one_h));
+				clicked = false;
+				Debug.Log ("clicked");
+
+				if (go && atPosition && go.GetComponent<drinkObject>() ) {
+					if (string.IsNullOrEmpty(one_h)) {
+						//Debug.Log ("test");
+						one_h = go.GetComponent<drinkObject> ().name;
+						//create go_1h
+						//go_1h = temp_plate;
+						go.GetComponent<drinkObject> ().numberOfDrinks -= 1;
+						go_1h = Instantiate (go.GetComponent<nameAndPosition>().go, transform.position + Vector3.right/2 + Vector3.down *1/2, transform.rotation) as GameObject;
+						go_1h.transform.SetParent (gameObject.transform);
+						//go_1h.gameObject.layer = 5;
+						Debug.Log (one_h);
+						
+					} else if (string.IsNullOrEmpty(two_h)) {
+						//Debug.Log ("HELLO?????????????");
+						two_h = go.GetComponent<drinkObject> ().name;
+						//create go_2h
+						//go_2h = temp_plate;
+						go.GetComponent<drinkObject> ().numberOfDrinks -= 1;
+						go_2h = Instantiate (go.GetComponent<nameAndPosition>().go, transform.position + Vector3.left * 2/3 + Vector3.down *1/2, transform.rotation) as GameObject;
+						//go_2h.gameObject.layer = 5;
+						go_2h.transform.SetParent (gameObject.transform);
+						
+					}
+				}
+
+
+				if (go && atPosition && go.GetComponent<cookingObject> ()) {
+					//Debug.Log(gameObject);
+					Debug.Log(go.GetComponent<cookingObject> ().canCook (one_h));
+					if (go.GetComponent<cookingObject> ().canCook (one_h) && 
+					    go.GetComponent<cookingObject> ().food_ready == false) {
+						Debug.Log ("cooking...");
+						go.GetComponent<cookingObject> ().cookReady = true;
+						go.GetComponent<cookingObject> ().chef_1h = one_h;
+						Destroy (go_1h);
+						one_h = "";
+					}
+					else if (go.GetComponent<cookingObject> ().canCook (two_h) && 
+					         go.GetComponent<cookingObject> ().food_ready == false) {
+						Debug.Log ("cooking...");
+						go.GetComponent<cookingObject> ().cookReady = true;
+						go.GetComponent<cookingObject> ().chef_1h = two_h;
+						Destroy (go_2h);
+						two_h = "";
+					}
+					
+					else if (go.GetComponent<cookingObject> ().food_ready && (string.IsNullOrEmpty(one_h) || string.IsNullOrEmpty(two_h)) ) {
+						
+						if (string.IsNullOrEmpty(one_h)) {
+							one_h = go.GetComponent<cookingObject> ().current_recipie.name;
+							
+							go_1h = Instantiate (go.GetComponent<cookingObject>().current_recipie.go, transform.position + Vector3.right/2 + Vector3.down *1/2, transform.rotation) as GameObject;
+							go_1h.transform.SetParent (gameObject.transform);
+							
+							Debug.Log ("picking up food...");
+							Debug.Log (go.GetComponent<cookingObject> ().food_cooking_name);
+							go.GetComponent<cookingObject> ().food_ready = false;
+						}
+						
+						else if (string.IsNullOrEmpty(two_h)) {
+							two_h = go.GetComponent<cookingObject> ().current_recipie.name;
+							
+							go_2h = Instantiate (go.GetComponent<cookingObject>().current_recipie.go, transform.position + Vector3.left * 2/3 + Vector3.down *1/2, transform.rotation) as GameObject;
+							go_2h.transform.SetParent (gameObject.transform);
+							
+							Debug.Log ("picking up food...");
+							Debug.Log (go.GetComponent<cookingObject> ().food_cooking_name);
+							go.GetComponent<cookingObject> ().food_ready = false;
+						}
+					}
+					
+					
+					//Debug.Log (other.gameObject.GetComponent<cookingObject> ().canCook (one_h, two_h));
+				} else if (go && atPosition && go.GetComponent<ingredientObject> ()) {
+					Debug.Log ("this is two_h: " + string.IsNullOrEmpty(two_h));
+					if (string.IsNullOrEmpty(one_h)) {
+						//Debug.Log ("test");
+						one_h = go.GetComponent<ingredientObject> ().name;
+						//create go_1h
+						//go_1h = temp_plate;
+						go_1h = Instantiate (go.GetComponent<nameAndPosition>().go, transform.position + Vector3.right/2 + Vector3.down *1/2, transform.rotation) as GameObject;
+						go_1h.transform.SetParent (gameObject.transform);
+						//go_1h.gameObject.layer = 5;
+						Debug.Log (one_h);
+						
+					} else if (string.IsNullOrEmpty(two_h)) {
+						//Debug.Log ("HELLO?????????????");
+						two_h = go.GetComponent<ingredientObject> ().name;
+						//create go_2h
+						//go_2h = temp_plate;
+						
+						go_2h = Instantiate (go.GetComponent<nameAndPosition>().go, transform.position + Vector3.left * 2/3 + Vector3.down *1/2, transform.rotation) as GameObject;
+						//go_2h.gameObject.layer = 5;
+						go_2h.transform.SetParent (gameObject.transform);
+						
+					}
+				} else if (go && atPosition && go.GetComponent<dropOffPoint> ()) {
+					if (hand_with_Food () == "one_h") {
+						//update dropoffpoint food name
+						if (string.IsNullOrEmpty (go.GetComponent<dropOffPoint> ().food_name)) {
+							go.GetComponent<dropOffPoint> ().food_name = one_h;
+							//add sprite of food
+							//delete 1h
+							one_h = "";
+						}
+					} else if (hand_with_Food () == "two_h") {
+						//update dropoffpoint food name
+						if (string.IsNullOrEmpty (go.GetComponent<dropOffPoint> ().food_name)) {
+							go.GetComponent<dropOffPoint> ().food_name = two_h;
+							//add sprite of food
+							//delete 1h
+							two_h = "";
+						}
+					}
+				} else if (go && atPosition && go.GetComponent<Customer> ()) {
+					//Debug.Log ("HEllO");
+					if (hand_with_Food () == "one_h") {
+						//update dropoffpoint food name
+						
+						if (string.IsNullOrEmpty (go.GetComponent<Customer> ().food_given)) {
+							go.GetComponent<Customer> ().food_given = one_h;
+							//add sprite of food
+							//delete 1h
+							Destroy (go_1h);
+							one_h = "";
+						}
+					} else if (hand_with_Food () == "two_h") {
+						//update dropoffpoint food name
+						if (string.IsNullOrEmpty (go.GetComponent<Customer> ().food_given)) {
+							go.GetComponent<Customer> ().food_given = two_h;
+							//add sprite of food
+							//delete 2h
+							Destroy (go_2h);
+							two_h = "";
+						}
+					}
+				} else if (go && atPosition) {
+					
+					if (!string.IsNullOrEmpty (two_h)) {
+						two_h = "";
+						Destroy (go_2h);
+					}
+					else if (!string.IsNullOrEmpty (one_h)) {
+						one_h = "";
+						Destroy (go_1h);
+					} 
+				}else {
+					//Debug.Log("didn't work!");
+					//Debug.Log(gameObject.tag);
+					clicked = true;
+				}
+
+
+				//When 1H = empty and 2H has food.
+				if (!string.IsNullOrEmpty(two_h) && string.IsNullOrEmpty(one_h)) {
+					one_h = two_h;
+					go_1h = Instantiate (go_2h, transform.position + Vector3.right/2 + Vector3.down *1/2, transform.rotation) as GameObject;
+					go_1h.transform.SetParent (gameObject.transform);
+					//------------------
+					Destroy (go_2h);
+					two_h = "";
+				}
+				
+			}
+		}
+		
+	}
+	
+	bool handsEmpty() {
+		if (string.IsNullOrEmpty(one_h) && string.IsNullOrEmpty(two_h))
+			return true;
+		else
+			return false;
+	}
+	
+	string hand_with_Food() {
+		if (!string.IsNullOrEmpty(one_h) && one_h.Contains("food")) {
+			return "one_h";
+		} else if (!string.IsNullOrEmpty(two_h) && two_h.Contains ("food")) {
+			return "two_h";
+		} return "";
+		//		if (one_h.Contains ("food")) {
+		//			return "one_h";
+		//		} else if (two_h.Contains ("food")) {
+		//			return "two_h";
+		//		}
+		//			return "";
+	}
+	
+	
+	GameObject findGameObjectAtClickedPosition() {
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("test")) {
+			if (go.GetComponent<nameAndPosition> ().x == mtX
+			    && go.GetComponent<nameAndPosition> ().y == mtY) {
+				return go;
+			}
+		}
+		return null;
+	}
+}
---
+++ Assets/Script/gameScene v2.0/drinkObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drinkObject : MonoBehaviour {
+	public int numberOfDrinks;
+	public string name;
+
+	public int maxDrinks;
+
+	private bool waitingOnDrink = false;
+	private int timeToMakeDrink = 5;
+
+	public Color c;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkDrinks ();
+	}
+	
+
+	void checkDrinks() {
+		if (!waitingOnDrink && numberOfDrinks < maxDrinks) {
+			Invoke("addDrink", timeToMakeDrink);
+			waitingOnDrink = true;
+		}
+	}
+
+	void addDrink() {
+		numberOfDrinks += 1;
+	}
+
+}
