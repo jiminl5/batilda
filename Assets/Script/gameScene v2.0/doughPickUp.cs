1b677fe7ae5d6185e5eb3dfcb4bce043c9e099e2
Chris Fulgencio <fulgencc@uci.edu>
2015-11-24 14:37:01 -0800

    dough sprite

--- Assets/Script/gameScene v2.0/doughPickUp.cs
+++ Assets/Script/gameScene v2.0/doughPickUp.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class doughPickUp : MonoBehaviour {
 	public int numberofDough;
+
+	private GameObject foodSprite;
 	// Use this for initialization
 	void Start () {
 		numberofDough = this.gameObject.GetComponentInParent<doughObject>().numberOfDough;
@@ -10,6 +12,14 @@ public class doughPickUp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		doughSprite ();
+	}
+
+	void doughSprite() {
+		if (numberofDough > 0 && !foodSprite) {
+			foodSprite = Instantiate (this.GetComponent<nameAndPosition> ().go, transform.position + Vector3.up / 2, transform.rotation) as GameObject;
+		} else if (numberofDough < 1) {
+			Destroy(foodSprite);
+		}
 	}
 }
