97d34b790dad24ca36b6946a358d87400abbea3c
jiminl5 <jiminl5@users.noreply.github.com>
2016-01-14 19:20:15 -0800

    Implemented Loading Screen - (e.g. Level Select -> Loading Screen -> Game Scene)

--- Assets/Script/MainMenu/LevelSelectionModal.cs
+++ Assets/Script/MainMenu/LevelSelectionModal.cs
@@ -4,7 +4,10 @@ using UnityEngine.Events;
 using System.Collections;
 
 public class LevelSelectionModal : MonoBehaviour {
-	
+
+    public GameObject LoadingScene;
+    public Image LoadingAsset;
+
     public void loadLevel(string level_number)
     {
         Application.LoadLevel("level" + level_number);
@@ -12,7 +15,16 @@ public class LevelSelectionModal : MonoBehaviour {
     
     public void loadScene(string scene_name)
     {
-        Application.LoadLevel(scene_name);
+        //Application.LoadLevel(scene_name);
+        StartCoroutine(LevelCoroutine(scene_name));
+    }
+
+    IEnumerator LevelCoroutine(string scene_name)
+    {
+        //LoadingScene.SetActive(true);
+        AsyncOperation async = Application.LoadLevelAsync(scene_name);
+        async.allowSceneActivation = true;
+        yield return async;
     }
 
     public void quitApp()
