4f34f8637dcb9e430249c05c72c7aa8b0be8f850
jiminl5 <jiminl5@users.noreply.github.com>
2016-01-19 15:13:16 -0800

    OpenSign - Bug Fix

--- Assets/Script/gameScene v2.0/Dialogue/OpenSignAnim.cs
+++ Assets/Script/gameScene v2.0/Dialogue/OpenSignAnim.cs
@@ -3,40 +3,43 @@ using System.Collections;
 
 public class OpenSignAnim : MonoBehaviour {
 
-    private bool start = false;
-    private float count = 1.0f;
-    private bool up = false;
-    private float bounce_h = 7.5f;
+    private int count;
+    bool up = false;
+    private float bounce_h = 5.5f;
     private float drop_speed = 10.0f;
-    private float depth = 4.215f;
+    private float depth = 4.0f;
     private float pause_time = 0.0001f;
     private float unpause_time = 10000f;
-    private float pauseTimer;
 	// Use this for initialization
 	void Start () {
         //start = true;
-        pauseTimer = 0.0f;
+        up = false;
+        count = 0;
         Time.timeScale = pause_time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (this.gameObject.transform.position.y > depth && count < 4.0f && !up) //drop
+        if (this.gameObject.transform.position.y < 0.0f) //Somehow position y has weird negative number?? (bug)
+        {
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 11.0f,0.0f);
+        }
+	    if (this.gameObject.transform.position.y >= depth && count < 4 && !up) //drop
         {
             this.transform.Translate(Vector2.down * drop_speed * (Time.deltaTime * unpause_time));
-            if (this.gameObject.transform.position.y < depth)
+            if (this.gameObject.transform.position.y < depth && !(this.gameObject.transform.position.y < 0.0f))
                 up = true;
         }
         if (up) //bounce up
         {
             this.transform.Translate(Vector2.up * drop_speed * (Time.deltaTime * unpause_time));
-            if (this.gameObject.transform.position.y > (bounce_h - count))
+            if (this.gameObject.transform.position.y > (bounce_h - count) )
             {
                 up = false;
                 count++;
             }
         }
-        if (count == 4.0f && (pauseTimer + 2.5f <= Time.time * unpause_time))
+        if (count == 4 && (2.5f <= Time.timeSinceLevelLoad * unpause_time))
         {
             this.transform.Rotate(new Vector3(0, 0, 45) * (Time.deltaTime * unpause_time));
             this.transform.Translate(Vector2.right * drop_speed * (Time.deltaTime * unpause_time));
