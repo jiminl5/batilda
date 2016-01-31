6046ce631d590b9672533055c4e2d3a025f65888
Warren Urbina <wurbina@uci.edu>
2016-01-31 11:49:18 -0800

    closer to transitioning between scenes

--- Assets/Script/SoundScripts/MusicSingleton.cs
+++ Assets/Script/SoundScripts/MusicSingleton.cs
@@ -2,58 +2,58 @@
 using System.Collections;
 
 public class MusicSingleton : MonoBehaviour {
-    private static bool new_level_loaded = false;
-    private static float fade_song_speed = .35f;
+	private static bool new_level_loaded = false;
+	private static int songs_level;
+	private static AudioClip song_for_level;
 
-    private static int scene_level_num;
-    private AudioClip this_levels_audio_clip;
-    private AudioClip level_loaded_audio_clip;
+	private static float fade_song_speed = .5f;
 
-    private static MusicSingleton instance = null;
+	private static MusicSingleton instance = null;
 
-    public static MusicSingleton Instance
-    {
-        get { return instance; }
-    }
+	public static MusicSingleton Instance
+	{
+		get { return instance; }
+	}
 
-    void Awake()
-    {
-        int scene_level_num = Application.loadedLevel;
-        this_levels_audio_clip = this.gameObject.GetComponent<AudioSource>().clip;
-        print(this_levels_audio_clip.name);
-        if (instance != null && instance != this)
-        {
-            level_loaded_audio_clip = this.gameObject.GetComponent<AudioSource>().clip;
-            print(level_loaded_audio_clip.name);
-            return;
-        }
-        else
-        {
-            instance = this;
-        }
-        DontDestroyOnLoad(this.gameObject);
-    }
+	void Start()
+	{
+		songs_level = Application.loadedLevel;
+		song_for_level = this.gameObject.GetComponent<AudioSource> ().clip;
+	}
 
-    void OnLevelWasLoaded(int level)
-    {
-        if (level != scene_level_num)
-        {
-            new_level_loaded = true;
-        }
-    }
-
-    void Update()
-    {
-        if (new_level_loaded && this_levels_audio_clip != level_loaded_audio_clip)
-        {
-            this.gameObject.GetComponent<AudioSource>().volume -= fade_song_speed * Time.deltaTime;
-            if(this.gameObject.GetComponent<AudioSource>().volume <= 0)
-            {
-                new_level_loaded = false;
-                Destroy(this.gameObject);
-            }
-        }
-    }
-
-
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	void OnLevelWasLoaded(int level)
+	{
+		if (level != songs_level)
+		{
+			new_level_loaded = true;
+		}
+	}
+	
+	void Update()
+	{
+		if (new_level_loaded && song_for_level != this.gameObject.GetComponent<AudioSource>().clip)
+		{
+			this.gameObject.GetComponent<AudioSource>().volume -= fade_song_speed * Time.deltaTime;
+			if(this.gameObject.GetComponent<AudioSource>().volume <= 0)
+			{
+				new_level_loaded = false;
+				Destroy(this.gameObject);
+			}
+		}
+	}
+	
+	
 }
