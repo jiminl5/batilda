184156ab139b8f3e6cdf0f78d0cd9b22850923e2
Ji Min (Jimmy) Lee <jiminl5@users.noreply.github.com>
2016-02-28 20:08:02 -0800

    Progress Bar stop (when furnace is off)

--- Assets/Script/gameScene v2.0/ProgressBar/timerBar.cs
+++ Assets/Script/gameScene v2.0/ProgressBar/timerBar.cs
@@ -6,17 +6,28 @@ public class timerBar : MonoBehaviour {
 
     public Transform timeBar;
     float temp_Time;
+    float temp_Time_1;
     //public Recipie current_recipe;
     // Use this for initialization
     public float cookingTime;
+
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        temp_Time += Time.deltaTime;
-        timeBar.GetComponent<Image>().fillAmount = temp_Time / cookingTime;
+
+    // Update is called once per frame
+    void Update() {
+        if (GameObject.Find("furnace").GetComponent<Furnace>().isOn)
+        { 
+            temp_Time += Time.deltaTime;
+            timeBar.GetComponent<Image>().fillAmount = temp_Time / cookingTime;
+        }
+        else if (GameObject.Find("furnace").GetComponent<Furnace>().isOn)
+        {
+            temp_Time = temp_Time_1;
+            timeBar.GetComponent<Image>().fillAmount = temp_Time / cookingTime;
+        }
+        temp_Time_1 = temp_Time;
         if (timeBar.GetComponent<Image>().fillAmount == 1)
         {
             Destroy(this.transform.parent.parent.gameObject);
