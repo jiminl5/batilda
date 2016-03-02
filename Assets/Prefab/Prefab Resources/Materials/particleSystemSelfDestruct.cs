f895b83528bb25c6152669543ca5a64e69634897
Ji Min (Jimmy) Lee <jiminl5@users.noreply.github.com>
2016-03-01 21:06:16 -0800

    New Folder (Prefab Resources)

---
+++ Assets/Prefab/Prefab Resources/Materials/particleSystemSelfDestruct.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleSystemSelfDestruct : MonoBehaviour {
+
+	private ParticleSystem ps;
+	
+	////////////////////////////////////////////////////////////////
+	
+	void Start () {
+		ps = this.GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+		if(ps)
+		{
+			if(!ps.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
