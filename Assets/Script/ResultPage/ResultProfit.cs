d2e786ca60099f24418142e49c5898cdb9ff8e0f
Ji Min (Jimmy) Lee <jiminl5@users.noreply.github.com>
2016-03-07 23:51:08 -0800

    Profit

---
+++ Assets/Script/ResultPage/ResultProfit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResultProfit : MonoBehaviour {
+
+    public Text profit_text;
+    private bool profit_text_change;
+	// Use this for initialization
+	void Start () {
+        profit_text_change = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (this.isActiveAndEnabled && profit_text_change)
+        {
+            profit_text.text = "Profit: <color=#338D74><b>" + PlayerPrefs.GetInt("temp_coin") + "</b></color>g";
+            profit_text_change = false;
+        }
+	}
+}
