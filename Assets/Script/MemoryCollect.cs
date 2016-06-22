03d0a27f9502b46181394669a8c3108ac6e802c0
Ji Min (Jimmy) Lee <jiminl5@users.noreply.github.com>
2016-06-21 17:28:24 -0700

    Texture Memory

---
+++ Assets/Script/MemoryCollect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCollect : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        if (Time.frameCount % 30 == 0)
+        {
+            System.GC.Collect();
+        }
+	}
+}
